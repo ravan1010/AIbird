5d7406228a657cfae40516bbf02079aa670877c0
ravan1010 <ravanravana177@gmail.com>
2025-11-22 19:50:20 +0530

    263

--- frontend/src/hooks/eyeblinkcount.js
+++ frontend/src/hooks/eyeblinkcount.js
@@ -2,16 +2,16 @@ import React, { useEffect, useRef, useState } from "react";
 import { FaceMesh } from "@mediapipe/face_mesh";
 import { Camera } from "@mediapipe/camera_utils";
 
-// =============== LEFT EYE DETECTION HOOK ===============
+// =============== EYE DETECTION HOOK ===============
 export default function useRightEyeControl() {
   const videoRef = useRef(null);
-  const [isRightEyeClosed, setIsRightEyeClosed ] = useState(false);
+  const [isRightEyeClosed, setIsRightEyeClosed] = useState(false);
 
   useEffect(() => {
     if (!videoRef.current) return;
 
     const faceMesh = new FaceMesh({
-      locateFile: (file) =>
+      locateFile: file =>
         `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`,
     });
 
@@ -20,29 +20,29 @@ export default function useRightEyeControl() {
       refineLandmarks: true,
       minDetectionConfidence: 0.5,
       minTrackingConfidence: 0.5,
-    }
+    });
 
-    faceMesh.onResults((results) => {
+    faceMesh.onResults(results => {
       if (!results.multiFaceLandmarks?.length) return;
 
       const lm = results.multiFaceLandmarks[0];
 
-      // LEFT EYE landmark indices
-      const L_TOP = lm[386];
-      const L_BOTTOM = lm[374];
-      const L_LEFT = lm[263];
-      const L_RIGHT = lm[362];
+      // Right eye landmark indices
+      const R_TOP = lm[386];
+      const R_BOTTOM = lm[374];
+      const R_LEFT = lm[263];
+      const R_RIGHT = lm[362];
 
       const dist = (a, b) =>
         Math.hypot(a.x - b.x, a.y - b.y);
 
       const ear =
-        dist(L_TOP, L_BOTTOM) /
-        dist(L_LEFT, L_RIGHT);
+        dist(R_TOP, R_BOTTOM) /
+        dist(R_LEFT, R_RIGHT);
 
-      const BLINK_TH = 0.25;
+      const BLINK_TH = 0.26;
+      console.log("BLINK_TH:", BLINK_TH, "EAR:", ear);
 
-      console.log("LEFT_EAR:", ear);
       setIsRightEyeClosed(ear <= BLINK_TH);
     });
 
