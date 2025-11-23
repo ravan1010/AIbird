c949661dc5fbc7eb6036c5d321ef644883df1b3f
ravan1010 <ravanravana177@gmail.com>
2025-11-22 17:11:23 -0800

    main

--- frontend/src/hooks/eyeblinkcount.js
+++ frontend/src/hooks/eyeblinkcount.js
@@ -40,7 +40,7 @@ export default function useRightEyeControl() {
         dist(R_TOP, R_BOTTOM) /
         dist(R_LEFT, R_RIGHT);
 
-      const BLINK_TH = 0.21;
+      const BLINK_TH = 0.35;
 
       console.log('BLINK_TH:', BLINK_TH, 'EAR:', ear);
 
